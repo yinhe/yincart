1638263e79351d7a65c8c77ea67d291c2de9554d
kowloon29320 <kowloon29320@163.com>
2013-06-22 15:25:19 +0800

    修改funcs

--- common/components/funcs.php
+++ common/components/funcs.php
@@ -2,18 +2,17 @@
 
 //打印并高亮函数
 function dump($target,$bool=true){
-	static $i = 0;
-	if($i == 0){
-		header("Content-type:text/html;charset=utf-8");
-	}
-    
+    static $i = 0;
+    if($i==0){
+        header('content-type:text/html;charset=utf-8');
+    }
     CVarDumper::dump($target, 10, true); 
-	
+    $i++;
     if($bool){
        exit; 
     }else{
-		$i++;
-		echo '<br />';
-	}
+        echo '<br />';
+    }
 }
+
 ?>
