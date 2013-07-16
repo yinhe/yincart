d058935e5604d603bfb670ad9a9748b1016a899a
Yin he <yhxxlm@gmail.com>
2013-07-16 16:59:11 +0800

    sql data update

--- advanced/common/components/F.php
+++ advanced/common/components/F.php
@@ -4,13 +4,12 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 class F {
-
+    
     static public function hello() {
 	echo 'hello';
     }
-
+    
     static public function rand_color() {
 	for ($a = 0; $a < 6; $a++) {    //采用#FFFFFF方法，  
 	    $d.=dechex(rand(0, 15)); //累加随机的数据--dechex()将十进制改为十六进制  
--- basic/protected/config/console.php
+++ basic/protected/config/console.php
@@ -11,19 +11,19 @@ return array(
 
 	// application components
 	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+//		'db'=>array(
+//			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+//		),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=yincart',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
