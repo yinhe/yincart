b36c77e97a983c226d64b559ad18c09889bf96eb
kowloon29320 <kowloon29320@163.com>
2013-09-04 23:38:24 +0800

    合同附件

---
+++ advanced/backend/models/ARGrouponAttach.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of ARGrouponAttach
+ * 团购合同附件表
+ * @author Jiulong Zhang <kowloon29320@163.com>
+ */
+class ARGrouponAttach extends DBGrouponAttach{
+    
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+}
+
+?>
