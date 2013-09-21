e09cb889af38b19581fb941a4f65f3c3fa5bce40
raiden <raiden@AY1309141531520666d9Z.(none)>
2013-09-21 22:30:23 +0800

    fix: issues#10

--- basic/protected/modules/user/views/admin/_search.php
+++ basic/protected/modules/user/views/admin/_search.php
@@ -1,53 +1,33 @@
 <div class="wide form">
 
-<?php $form=$this->beginWidget('CActiveForm', array(
-    'action'=>Yii::app()->createUrl($this->route),
-    'method'=>'get',
+<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+	'action'=>Yii::app()->createUrl($this->route),
+	'method'=>'get',
 )); ?>
 
-    <div class="row">
-        <?php echo $form->label($model,'id'); ?>
-        <?php echo $form->textField($model,'id'); ?>
-    </div>
+<?php echo $form->textFieldRow($model,'id'); ?>
 
-    <div class="row">
-        <?php echo $form->label($model,'username'); ?>
-        <?php echo $form->textField($model,'username',array('size'=>20,'maxlength'=>20)); ?>
-    </div>
+<?php echo $form->textFieldRow($model,'username',array('size'=>20,'maxlength'=>20)); ?>
 
-    <div class="row">
-        <?php echo $form->label($model,'email'); ?>
-        <?php echo $form->textField($model,'email',array('size'=>60,'maxlength'=>128)); ?>
-    </div>
+<?php echo $form->textFieldRow($model,'email',array('size'=>60,'maxlength'=>128)); ?>
 
-    <div class="row">
-        <?php echo $form->label($model,'activkey'); ?>
-        <?php echo $form->textField($model,'activkey',array('size'=>60,'maxlength'=>128)); ?>
-    </div>
+<?php echo $form->textFieldRow($model,'activkey',array('size'=>60,'maxlength'=>128)); ?>
 
-    <div class="row">
-        <?php echo $form->label($model,'create_at'); ?>
-        <?php echo $form->textField($model,'create_at'); ?>
-    </div>
+<?php echo $form->textFieldRow($model,'create_at'); ?>
 
-    <div class="row">
-        <?php echo $form->label($model,'lastvisit_at'); ?>
-        <?php echo $form->textField($model,'lastvisit_at'); ?>
-    </div>
+<?php echo $form->textFieldRow($model,'lastvisit_at'); ?>
 
-    <div class="row">
-        <?php echo $form->label($model,'superuser'); ?>
-        <?php echo $form->dropDownList($model,'superuser',$model->itemAlias('AdminStatus')); ?>
-    </div>
+<?php echo $form->dropDownListRow($model,'superuser',$model->itemAlias('AdminStatus')); ?>
 
-    <div class="row">
-        <?php echo $form->label($model,'status'); ?>
-        <?php echo $form->dropDownList($model,'status',$model->itemAlias('UserStatus')); ?>
-    </div>
+<?php echo $form->dropDownListRow($model,'status',$model->itemAlias('UserStatus')); ?>
 
-    <div class="row buttons">
-        <?php echo CHtml::submitButton(UserModule::t('Search')); ?>
-    </div>
+<div class="form-actions">
+<?php $this->widget('bootstrap.widgets.TbButton', array(
+	'buttonType' => 'submit',
+	'type'=>'primary',
+	'label'=>'Search',
+)); ?>
+</div>
 
 <?php $this->endWidget(); ?>
 
